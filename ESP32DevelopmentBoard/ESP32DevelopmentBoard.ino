cd3d3b771cdee5371c1b3570d65f06330c366a41
Renan Maitan <renanmaitan12@gmail.com>
2023-05-25 17:00:37 -0300

    v1
    Primeiro push

---
+++ ESP32DevelopmentBoard/ESP32DevelopmentBoard.ino
@@ -0,0 +1,100 @@
+// This example uses an ESP32 Development Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://www.shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include "WiFiEsp.h" //INCLUSÃO DA BIBLIOTECA
+#include "SoftwareSerial.h"//INCLUSÃO DA BIBLIOTECA
+#include <MQTT.h>
+#include <ArduinoJson.h> // Inclua a biblioteca ArduinoJson
+
+SoftwareSerial Serial1(6, 7); //PINOS QUE EMULAM A SERIAL, ONDE O PINO 6 É O RX E O PINO 7 É O TX
+
+char ssid[] = "Renan_2.4G"; //VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO
+char pass[] = "ram998451";//VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO
+
+WiFiEspClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+float umidade = 0;
+float luminosidade = 1;
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("mqtt/request");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  
+  // Verifica se o tópico recebido é o "mqtt/request"
+  if (topic == "mqtt/request") {
+    // Faça o processamento necessário para a mensagem recebida
+    // Exemplo: Verificar o conteúdo da mensagem e executar ações correspondentes
+    
+    // Exiba a mensagem no Serial do Arduino
+    Serial.println("Received message from mqtt/request: " + payload);
+  }
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600); //INICIALIZA A SERIAL PARA O ESP8266
+  WiFi.init(&Serial1); //INICIALIZA A COMUNICAÇÃO SERIAL COM O ESP8266
+  WiFi.config(IPAddress(192,168,0,110)); //COLOQUE UMA FAIXA DE IP DISPONÍVEL DO SEU ROTEADOR
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  client.begin("ec2-18-218-179-131.us-east-2.compute.amazonaws.com", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+  umidade++;
+  luminosidade++;
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    StaticJsonDocument<200> jsonDocument;
+    jsonDocument["umidade"] = umidade;
+    jsonDocument["luminosidade"] = luminosidade;
+
+    // Conversão do objeto JSON para uma string
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
+
+    // Publicação da mensagem JSON no tópico MQTT
+    client.publish("mqtt/leituras", jsonString);
+  }
+}
---
+++ app/main.py
@@ -0,0 +1,83 @@
+import random
+from paho.mqtt import client as mqtt_client
+import json
+import time
+import http.client
+from datetime import datetime
+
+client = http.client.HTTPConnection('ec2-18-218-179-131.us-east-2.compute.amazonaws.com', 1883)
+client.connect()
+client.publish('/planta', 'Ligar Bomba')
+
+broker = 'ec2-18-218-179-131.us-east-2.compute.amazonaws.com'
+port = 1883
+topic = "mqtt/request"
+topic_leituras = "mqtt/leituras"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'emqx'
+password = 'public'
+device_id = 'device_1'
+ledOn = False;
+bombaOn = False;
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        # if not isBombaOn:
+        #     client.publish(topic, 1) 
+        y = json.loads(msg.payload.decode())
+        umidade = float((y['umidade']))
+        luminosidade = float((y['luminosidade']))
+        print(time.time)
+        # tipoPlanta = y['tipoPlanta']
+        # luminosidadeIdeal = y['luminosidadeIdeal']
+        # umidadeIdeal = y['umidadeIdeal']
+        # isLedOn = y['isLedOn']
+        # isBombaOn = y['isBombaOn']
+        
+        # if luminosidade < luminosidadeIdeal:
+            # if not isLedOn:
+            #   client.publish(topic, 0)
+            #   ledOn = True
+        
+        # if umidade < umidadeIdeal:
+            # if not isBombaOn:
+            #   client.publish(topic, 1)
+            #   bombaOn = True
+        
+        print(umidade)
+        print(luminosidade)
+        #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+    
+    client.subscribe(topic_leituras)
+    client.on_message = on_message
+    
+    
+    
+def publish(client, id):
+    if id == 0:
+        result = client.publish(topic,"Ligar Bomba")
+    else:
+        result = client.publish(topic,"Ligar Led")
+    
+def main():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+    
+if __name__ == '__main__':
+    main()
+    
